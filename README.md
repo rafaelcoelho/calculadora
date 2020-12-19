debcc744737bc829b09af60818e10f73574462f9
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2020-12-19 19:51:27 -0300

    Primeira versão da calculadora da Carol
    - Operação Adição
    - Operação de Subtração
    - Verificando se a operação eh válida

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # calculadora
 Esta é a Calculadora da Feita pela Carol (Minha Filha Amada)
+
+
+## Algoritmo
+- Passo 0: ligar a calculadora
+- Passo 1: digitar o primeiro número
+- Passo 2: selecionar a operação (Adição/Subtração)
+- Passo 3: digitar o segundo número
+- Passo 4: selecionar o sinal de igual
+- Passo 5: mostrar o resultado
---
+++ src/index.js
@@ -0,0 +1,42 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+bemVindo()
+
+rl.question("Digite a operação desejada: ", function (operacao) {
+    rl.question("Digite o primeiro numero: ", function (primeiroNumero) {
+        rl.question("Digite o segundo numero: ", function (segundoNumero) {
+            if (operacao == 1) {
+                adicao(parseInt(primeiroNumero), parseInt(segundoNumero))
+            } else if (operacao == 2) {
+                subtracao(parseInt(primeiroNumero), parseInt(segundoNumero))
+            } else {
+                console.log('Operação Inválida !!!')
+            }
+            rl.close();
+        });
+    });
+});
+
+function bemVindo() {
+    console.log('Bem vindo a calculadora da Carol')
+
+    console.log('\n1 - Operação de Adição')
+    console.log('2 - Operação de Subtração')
+}
+
+function adicao(numero1, numero2) {
+    let resultado = numero1 + numero2
+
+    console.log(`O resultado da operação é ${resultado}`)
+}
+
+function subtracao(primeiroNumero, segundoNumero) {
+    let resultado = primeiroNumero - segundoNumero
+
+    console.log(`O resultado da operação é ${resultado}`)
+}
